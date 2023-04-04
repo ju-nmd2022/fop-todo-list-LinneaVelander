6d450836dc86b644b41523e4eed5bbdbbb8b7ace
Linnea <veli22ts@student.ju.se>
2023-04-04 17:25:21 +0200

    started with javascript

--- todoList.js
+++ todoList.js
@@ -1,6 +1,25 @@
-// function onLoadHandler() {
-//   // Add elements that are made during the visit on the website, for example a list
-// }
+function onLoadHandler() {
+  // Add elements that are made during the visit on the website, for example a list
+}
 
-// window.addEventListener("load", onLoadHandler);
+window.addEventListener("load", onLoadHandler);
 
+// Finished button (a circle) with a line through the text
+// Create a new list item when clicking the + button
+// Be able to delete tasks
+
+const addNewTasksButton = document.getElementById("toDoListButton");
+const inputForNewTasks = document.getElementById("inputForNewTasks");
+const toDoList = document.getElementById("toDoList");
+const toDoListTasks = document.getElementById("toDoListTasks");
+
+let newToDoTasks = [];
+
+addNewTasksButton.addEventListener("click", addNewTasks);
+
+function addNewTasks() {
+  if (inputForNewTasks.value.length > 0) {
+    const listOfNewTasks = document.createElement("li");
+    listOfNewTasks.appendChild(toDoListTasks);
+  }
+}
