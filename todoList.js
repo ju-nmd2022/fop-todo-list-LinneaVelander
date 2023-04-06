4c719667add73ea34a04ce74dc038ea9d5b9f259
Linnea <veli22ts@student.ju.se>
2023-04-06 10:47:29 +0200

    more local storage and fixed the delete task fun

--- todoList.js
+++ todoList.js
@@ -9,13 +9,20 @@ const inputForNewTasks = document.getElementById("inputForNewTasks");
 const toDoList = document.getElementById("toDoList");
 const toDoListTasks = document.getElementById("toDoListTasks");
 
+const TO_DO_KEY = "toDoKey";
+
 // Array for all tasks, empty by default
 let toDoTasks = [];
 
-// The plus button to add tasks to the list
-addNewTasksButton.addEventListener("click", () => {
-  createTask();
-});
+// fetchLocalStorage();
+
+// The plus button to add tasks to the list.
+// If input field length is larger than 0 you may add your task
+if (inputForNewTasks.length > 0) {
+  addNewTasksButton.addEventListener("click", () => {
+    createTask();
+  });
+}
 
 // Function to add create tasks in the array
 function createTask() {
@@ -30,63 +37,85 @@ function createTask() {
   // To add object to array
   toDoTasks.push(newToDo);
 
+  sortToDos();
+
+  inputForNewTasks.value = "";
+  saveLocalStorage();
+
+  // Local Storage. Set the item to the name of the to do
+  // Stringify since it is an object and local storage only can store strings
+}
+
+function saveLocalStorage() {
+  let json = JSON.stringify(toDoTasks);
+  localStorage.setItem(TO_DO_KEY, json);
+}
+
+function fetchLocalStorage() {
+  let json = localStorage.getItem(TO_DO_KEY);
+
+  if (json != null) {
+    let temp = JSON.parse(json);
+    toDoTasks = temp;
+    refreshList();
+  }
+}
+
+// Draws the list in ul element
+function drawToDo(toDo) {
   const toDoElement = document.createElement("li");
   const toDoParagraph = document.createElement("p");
 
   // The name (text written in input) of the to do task
   // The names will be put in a paragraph that is displayed as an li element
-  toDoParagraph.innerText = newToDo.name;
+  toDoParagraph.innerText = toDo.name;
 
   const radioButton = document.createElement("input");
   radioButton.type = "radio";
-  radioButton.checked = false;
+  radioButton.checked = toDo.done;
 
   // References to elements in order to access them
-  newToDo.radioBtn = radioButton;
-  newToDo.nameElement = toDoParagraph;
-  newToDo.parent = toDoElement;
+  toDo.radioBtn = radioButton;
+  toDo.nameElement = toDoParagraph;
+  toDo.parent = toDoElement;
 
-  // Parameter newToDo to know which object is clicked
+  if (toDo.done) {
+    toDo.nameElement.style.textDecoration = "line-through";
+  }
+
+  // Parameter toDo to know which object is clicked
   radioButton.addEventListener("click", () => {
-    checkedToDo(newToDo);
+    checkedToDo(toDo);
   });
 
   const trashcanIcon = document.createElement("img");
   trashcanIcon.src = "trashcan.svg";
 
-  // Parameter newToDo to know which object is clicked
+  // Parameter toDo to know which object is clicked
   trashcanIcon.addEventListener("click", () => {
-    deleteToDo(newToDo);
+    deleteToDo(toDo);
   });
 
   toDoElement.appendChild(radioButton);
   toDoElement.appendChild(toDoParagraph);
   toDoElement.appendChild(trashcanIcon);
-  drawToDo(newToDo);
-
-  inputForNewTasks.value = "";
-
-  // Local Storage. Set the item to the name of the to do
-  // Stringify since it is an object and local storage only can store strings
-  localStorage.setItem(newToDo.name, JSON.stringify(newToDo));
-  localStorage.getItem(newToDo);
-
-  console.log(localStorage);
-}
-
-// Draws the list in ul element
-function drawToDo(toDo) {
   toDoListTasks.appendChild(toDo.parent);
 }
 
 function checkedToDo(toDo) {
-  toDo.nameElement.style.textDecoration = "line-through";
-  toDo.done = true;
+  toDo.done = !toDo.done;
+  saveLocalStorage();
   sortToDos();
 }
 
+// To find the index in order to remove the to do task.
 function deleteToDo(toDo) {
+  let index = toDoTasks.findIndex((toDoTask) => toDoTask.name === toDo.name);
+  if (index > -1) {
+    toDoTasks.splice(index, 1);
+  }
   toDoListTasks.removeChild(toDo.parent);
+  saveLocalStorage();
 }
 
 function sortToDos() {
